3edc4f4f59b11095b10d43a92f3dfc7bf0d876b6
duxa4854 <nzaichenko531@gmail.com>
2026-03-17 13:21:58 +0300

    Step 8: explicit and implicit type conversion

--- main.js
+++ main.js
@@ -95,3 +95,27 @@ console.log(typeof function () {});
 let numberX = null;
 console.log(numberX === null);
 
+let num = 42;
+let str = String(num);
+let str2 = num.toString();
+let str3 = "" + num;
+
+let strNum = "123";
+let int = Number(strNum);
+let int2 = parseInt("42.5")
+let float = parseFloat("3.14");
+let int3 = +"99";
+
+let bool1 = Boolean(1);
+let bool2 = !!1;
+let bool3 = Boolean(0);
+let bool4 = Boolean("");
+
+console.log("5" + 3);
+console.log("5" - 3);
+console.log("5" * "2");
+console.log(true + 1);
+console.log(false + 1);
+console.log(null + 1);
+console.log(undefined + 1);
+
